011668c729aa78b351ca53f08b3f37590b6ff7cf
nboehning <nboehning@gmail.com>
2015-10-23 12:43:58 -0500

    More lobby updating
    More lobby updating

---
+++ Project-05--SettlersOfNotCatanMP/Assets/Scripts/Common/SettlersOfNotCatanLobbyHook.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Network;
+
+public class SettlersOfNotCatanLobbyHook : LobbyHook
+{
+    public override void OnLobbyServerSceneLoadedForPlayer(UnityEngine.Networking.NetworkManager manager, GameObject lobbyPlayer, GameObject gamePlayer)
+    {
+        LobbyPlayer l = lobbyPlayer.GetComponent<LobbyPlayer>();
+
+        ScriptPlayer paddle = gamePlayer.GetComponent<ScriptPlayer>();
+
+        PongManager.AddPlayer(paddle);
+    }
+}
