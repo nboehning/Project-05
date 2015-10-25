ab8739eacb5ebd3096d7873baeef3353d098397e
nboehning <nboehning@gmail.com>
2015-10-25 02:36:21 -0500

    Did more networking updates
    Did more updates to the lobby

---
+++ Project-05--SettlersOfNotCatanMP/Assets/Scripts/Common/ScriptCatanLobbyHook.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UnityStandardAssets.Network
+{
+    public class CatanLobbyHook : LobbyHook
+    {
+        public override void OnLobbyServerSceneLoadedForPlayer(UnityEngine.Networking.NetworkManager manager, GameObject lobbyPlayer, GameObject gamePlayer)
+        {
+            LobbyPlayer l = lobbyPlayer.GetComponent<LobbyPlayer>();
+
+            ScriptPlayer player = gamePlayer.GetComponent<ScriptPlayer>();
+
+            player.PlayerName = l.name;
+            //player.color = l.playerColor;
+            //paddle.playerName = l.playerName;
+
+            //PongManager.AddPlayer(paddle);
+        }
+    }
+}
